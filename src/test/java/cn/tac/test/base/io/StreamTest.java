b21cf79e96c9a853d80b2c4b3a1637351fcd472a
tac <514162920@qq.com>
2017-09-04 21:05:46 +0800

    charset

--- src/test/java/cn/tac/test/base/io/StreamTest.java
+++ src/test/java/cn/tac/test/base/io/StreamTest.java
@@ -2,7 +2,11 @@ package cn.tac.test.base.io;
 
 import org.junit.Test;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 
 /**
  * @author : tac
@@ -20,5 +24,30 @@ public class StreamTest extends BaseIOTest {
     public void testGBK() throws IOException {
         InputStream is = Class.class.getResourceAsStream("/assets/gbk.txt");
         print(is, "gbk");
+    }
+
+    @Test
+    public void testCharSet() {
+        Charset charset = Charset.forName("utf8");
+        if (!charset.canEncode()) {
+            throw new UnsupportedOperationException();
+        }
+        ByteBuffer byteBuffer = charset.encode("hello charset encoder, ^ ^ 这只是个测试");
+        System.out.println("buffer capacity: " + byteBuffer.capacity());
+        System.out.println("buffer remaining: " + byteBuffer.remaining());
+        while (byteBuffer.hasRemaining()) {
+            System.out.print(byteBuffer.get() + ", ");
+        }
+
+        System.out.println();
+        divider();
+
+        ByteBuffer byteBuffer1 = charset.encode("hello charset decoder, ^ ^ 这个是测试1");
+        CharBuffer charBuffer = charset.decode(byteBuffer1);
+        System.out.println("buffer capacity: " + charBuffer.capacity());
+        System.out.println("buffer remaining: " + charBuffer.remaining());
+        while (charBuffer.hasRemaining()) {
+            System.out.print(charBuffer.get());
+        }
     }
 }
